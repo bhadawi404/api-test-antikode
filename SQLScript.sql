63babeeabcfbb9881d3d04224a6390ef623a680c
bhadawi404 <abadawi.911@gmail.com>
2023-01-06 01:23:48 +0700

    SQL Script formatted

--- SQLScript.sql
+++ SQLScript.sql
@@ -1,23 +1,23 @@
 
-select 
-	b.name as brand_name 
-from brands b ;
+SELECT
+	b.name as brand_name
+FROM brands b ;
 
 
 
-select 
-	o.name as outlet_name,
-	o.address as address,
-	o.longitude as longitude,
-	o.latitude as latitude 
-from outlets o; 
+SELECT
+	o.name AS outlet_name,
+	o.address AS address,
+	o.longitude AS longitude,
+	o.latitude AS latitude
+FROM outlets o;
 
 
-select 
-	sum(p.price) as total_product
-	from products p ;
-	
-SELECT o.name as outlet_name,o.address as outlet_address,o.latitude as latitude ,o.longitude as longitude, SQRT(
+SELECT
+	sum(p.price) AS total_product
+	FROM products p ;
+
+SELECT o.name AS outlet_name,o.address AS outlet_address,o.latitude AS latitude ,o.longitude AS longitude, SQRT(
     POW(111.2 * (latitude - -6.17511506439825), 2)
     +
     POW(111.2 * (106.82746393252476 - longitude) * COS(latitude  / 57.3), 2)) AS distance
